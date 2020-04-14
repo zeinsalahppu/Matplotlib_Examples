b02a7f721e2c785669bd4b4287424eedb1eb8da1
zeinsalahppu <zeinsalah@gmail.com>
2020-04-14 21:42:25 +0300

    scatter plot

--- matplotlib_examples.py
+++ matplotlib_examples.py
@@ -7,21 +7,15 @@ simple matplotlib Examples
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-2 * np.pi, 2 * np.pi, 100)
-
-f_sin = 3*np.sin(x)
-f_poly = np.sin(2*x) - np.cos(x)
+x = [14.2, 16.4, 11.9, 15.2, 18.5, 22.1, 19.4, 25.1, 23.4, 18.1, 22.6, 17.2]
+y = [215, 325, 185, 332, 406, 522, 412, 614, 544, 421, 445, 408]
 
 fig, ax = plt.subplots()
 
-startx, endx = -2 * np.pi - 0.1, 2*np.pi + 0.1
-starty, endy = -4.0, 4.0
-ax.axis([startx, endx, starty, endy])
+ax.set(xlabel='Temperature',
+       ylabel='Ice Cream Sales',
+       title='Temperature/Ice Cream Correlation')
 
-ax.set(xlabel='x-axis',
-       ylabel='y-axis',
-       title='Some periodic functions')
-
-ax.plot(x, f_sin, '--', x, f_poly, 'or')
+ax.scatter(x, y)
 plt.show()
 
