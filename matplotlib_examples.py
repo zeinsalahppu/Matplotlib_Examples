e0288a4e033b735a4f31965f7f966667a6cbf549
zeinsalahppu <zeinsalah@gmail.com>
2020-04-14 11:09:29 +0300

    xlabel ylabel and title

--- matplotlib_examples.py
+++ matplotlib_examples.py
@@ -8,7 +8,17 @@ import matplotlib.pyplot as plt
 days = list(range(1, 8))
 temperature = [10, 14, 16, 20, 20, 14, 17]
 
-plt.plot(days, temperature,
+fig, ax = plt.subplots() # one axis
+
+# plt.xlabel('Days)')
+# plt.ylabel('Temperature')
+# plt.title('Temperature of the Week')
+
+ax.set(xlabel='Days', ylabel='Temperature',
+       title='Temperature of the Week')
+
+
+ax.plot(days, temperature,
          days, temperature, "or")
 
 plt.show()
