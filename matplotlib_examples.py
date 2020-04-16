cc9d2e524e364215cc29499eb7e2b7a1804fc6f0
zeinsalahppu <zeinsalah@gmail.com>
2020-04-17 00:23:48 +0300

    simple bar charts side-by-side

--- matplotlib_examples.py
+++ matplotlib_examples.py
@@ -1,14 +1,29 @@
 """
 simple matplotlib Examples
-
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 
-days = list(range(1, 8))
-temperature = [10, 14, 16, 20, 20, 14, 17]
 
-plt.plot(days, temperature,
-         days, temperature, "or")
+labels = ['Eng', 'IT', 'CAS', 'Sci', 'Med', 'CAP']
+male_count = [280, 250, 210, 130, 145, 500]
+female_count = [170, 270, 290, 150, 165, 350]
+
+x = np.arange(len(labels))
+width = 0.25
+
+fig, ax = plt.subplots()
+ax.bar(x - width/2, male_count, width, label='Male', color = "red")
+ax.bar(x + width/2, female_count, width, label='Female', color = "blue")
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('PPU Students')
+ax.set_title('Students per College')
+ax.set_xticks(x)
+ax.set_xticklabels(labels, rotation='vertical')
+ax.legend()
+
+fig.tight_layout()
 
 plt.show()
