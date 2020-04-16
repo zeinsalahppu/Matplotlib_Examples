29cbae0e1ffdffdfbd0467314438e38fb6fa4003
zeinsalahppu <zeinsalah@gmail.com>
2020-04-16 23:27:03 +0300

    annotations

--- matplotlib_examples.py
+++ matplotlib_examples.py
@@ -18,7 +18,7 @@ ax.spines['right'].set_visible(False)
 ax.spines['bottom'].set_position(('data', 0))
 ax.spines['left'].set_position(('data', 0))
 
-ax.set_xticks([-2.0 * np.pi, -1.5 * np.pi, -1.0 * np.pi, -0.5 * np.pi, 0,
+ax.set_xticks([-2.0 * np.pi, -1.5 * np.pi, -1.0 * np.pi, -0.5 * np.pi,
                 0.5 * np.pi, 1.0 * np.pi, 1.5 * np.pi, 2.0 * np.pi])
 ax.set_xticklabels([r'$-2\pi$', '', r'$-\pi$', '', '', r'$\pi$', '', r'$2\pi$'])
 
@@ -28,6 +28,20 @@ ax.set_title('Some periodic functions')
 
 ax.plot(x, f_sin, '--', label="3$sin(x)$")
 ax.plot(x, f_2, 'r', label="$sin(2x)-cos(x)$")
-ax.legend(loc='best')
+# ax.legend(loc='best')
+
+pnt = 2.3, 3*np.sin(2.3)
+ax.annotate("3$sin(x)$", xy= pnt,
+            xytext=(4.5, 3),
+            color="blue",
+            arrowprops=dict(arrowstyle="->", color="blue",
+                       connectionstyle="angle3,angleA=0,angleB=-90"))
+
+pnt = 4.2, np.sin(2* 4.2) - np.cos(4.2)
+ax.annotate("3$sin(x)$", xy= pnt,
+            xytext=(5.5, 2),
+            color="red",
+            arrowprops=dict(arrowstyle="->", color="red",
+                       connectionstyle="angle3,angleA=0,angleB=-90"))
 
 plt.show()
