a9e626193ba6a200869ac16e942f9993236a1073
zeinsalahppu <zeinsalah@gmail.com>
2020-04-17 20:55:54 +0300

    changed color map

--- matplotlib_examples.py
+++ matplotlib_examples.py
@@ -3,6 +3,7 @@ simple matplotlib Examples
 """
 
 import matplotlib.pyplot as plt
+import matplotlib.colors
 import numpy as np
 
 labels = ['Eng', 'IT', 'CAS', 'Sci', 'Med', 'CAP']
@@ -12,20 +13,20 @@ flattened_student_count = student_count.flatten()
 
 fig, ax = plt.subplots()
 
-# use a color map to define colors
-oc_idx = np.arange(len(aggregated_student_count))*4
-ic_idx = (oc_idx[: , np.newaxis] + np.array([1, 2])).flatten()
-print(oc_idx)
-print(ic_idx)
+# combined the color maps "tab20c" and "tab20b"
+combined_colors = np.vstack((plt.get_cmap("tab20c").colors, plt.get_cmap("tab20b").colors))
+cmap= matplotlib.colors.ListedColormap(combined_colors)
 
-cmap = plt.get_cmap("tab20c")
+# use the combined color map to define colors
+oc_idx = np.arange(len(aggregated_student_count))*4
+ic_idx = (oc_idx[: , np.newaxis] + np.array([1, 3])).flatten()
 outer_colors = cmap(oc_idx)
 inner_colors = cmap(ic_idx)
 
 size = 0.3
 ax.pie(aggregated_student_count, radius=1, colors=outer_colors, labels= labels, wedgeprops=dict(width=size, edgecolor='white'))
-
 ax.pie(flattened_student_count, radius=1-size, colors=inner_colors, wedgeprops=dict(width=size, edgecolor='white'))
 
-ax.set(aspect="equal", title='Pie plot with `ax.pie`')
+ax.set_title('University Students Distribution per College')
+
 plt.show()
