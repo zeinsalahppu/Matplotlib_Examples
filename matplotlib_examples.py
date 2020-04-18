3842b58cd4f6169fd962a5181fddda93bbff22e5
zeinsalahppu <zeinsalah@gmail.com>
2020-04-18 22:04:55 +0300

    more complex grids

--- matplotlib_examples.py
+++ matplotlib_examples.py
@@ -3,20 +3,21 @@ import matplotlib.gridspec as gridspec
 
 fig = plt.figure()
 
-gs0 = gridspec.GridSpec(1, 2, figure=fig)
-gs00 = gridspec.GridSpecFromSubplotSpec(3, 1, subplot_spec=gs0[0])
-gs01 = gridspec.GridSpecFromSubplotSpec(1, 1, subplot_spec=gs0[1])
+gs = gridspec.GridSpec(3, 3, figure=fig)
 
-ax1 = fig.add_subplot(gs00[0, 0])
+ax1 = plt.subplot(gs[0, :])
 ax1.text(0.5, 0.5, 'Axes 1', ha='center', va='center', size=24)
 
-ax2 = fig.add_subplot(gs00[1, 0])
+ax2 = fig.add_subplot(gs[1, :2])
 ax2.text(0.5, 0.5, 'Axes 2', ha='center', va='center', size=24)
 
-ax3 = fig.add_subplot(gs00[2, 0])
+ax3 = fig.add_subplot(gs[2, 0])
 ax3.text(0.5, 0.5, 'Axes 3', ha='center', va='center', size=24)
 
-ax4 = fig.add_subplot(gs01[0])
+ax4 = fig.add_subplot(gs[2, 1])
 ax4.text(0.5, 0.5, 'Axes 4', ha='center', va='center', size=24)
+
+ax5 = fig.add_subplot(gs[1:, 2])
+ax5.text(0.5, 0.5, 'Axes 5', ha='center', va='center', size=24)
 
 plt.show()
