a7f2f67d67963f5b10d1f5ddd0b02020c9e89add
zeinsalahppu <zeinsalah@gmail.com>
2020-04-18 11:43:58 +0300

    one row or one col plots

--- matplotlib_examples.py
+++ matplotlib_examples.py
@@ -3,25 +3,22 @@ simple matplotlib Examples
 
 """
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-2 * np.pi, 2 * np.pi, 100)
+f_sin = 3 * np.sin(x)
+f_2 = np.sin(2 * x) - np.cos(x)
 
-f_sin = 3*np.sin(x)
-f_poly = np.sin(2*x) - np.cos(x)
+fig, axs = plt.subplots(2, 1, sharex=True, sharey=True)
 
-fig, ax = plt.subplots()
+fig.suptitle('Some periodic functions')
 
-startx, endx = -2 * np.pi - 0.1, 2*np.pi + 0.1
-starty, endy = -4.0, 4.0
-ax.axis([startx, endx, starty, endy])
+axs[0].set_title('f(x)=3$sin(x)$')
+axs[0].plot(x, f_sin, '--', label="3$sin(x)$")
 
-ax.set(xlabel='x-axis',
-       ylabel='y-axis',
-       title='Some periodic functions')
+axs[1].set_title('f(x)=sin(2x)-cos(x)$')
+axs[1].plot(x, f_2, 'r', label="$sin(2x)-cos(x)$")
 
-ax.plot(x, f_sin, '--', x, f_poly, 'or')
 plt.show()
 
